5e4100ba21a0f1e8d5ec2a61d4cdc1a1e71e5369
Josh Rowley <joshua.f.rowley@gmail.com>
2012-12-11 10:44:16 -0500

    set oauth client options

--- lib/omniauth-trello.rb
+++ lib/omniauth-trello.rb
@@ -1,7 +1,16 @@
 require "omniauth-trello/version"
+require "omniauth-oauth"
 
 module Omniauth
   module Trello
-    # Your code goes here...
+    class Trello < OmniAuth::Strategies::OAuth
+      option :name, "Trello"
+      option :client_options, { :site => "https://trello.com",
+                                :request_token_path => "/1/OAuthGetRequestToken",
+                                :access_token_path => "/1/OAuthGetAccessToken",
+                                :authorize_path => "/1/OAuthAuthorizeToken" }
+
+      
+    end
   end
 end
