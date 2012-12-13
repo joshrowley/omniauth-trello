3025cb1f84ca7f4fab8cd56a93fdc3b8f3f60f97
Josh Rowley <joshua.f.rowley@gmail.com>
2012-12-12 19:39:24 -0500

    working class?

---
+++ lib/omniauth/strategies/trello.rb
@@ -0,0 +1,36 @@
+require "omniauth-trello/version"
+require 'omniauth-oauth'
+require 'multi_json'
+
+module OmniAuth
+  module Strategies
+    class Trello < OmniAuth::Strategies::OAuth
+      option :name, "trello"
+      option :client_options, { :site => "https://trello.com",
+                                :request_token_path => "/1/OAuthGetRequestToken",
+                                :access_token_path => "/1/OAuthGetAccessToken",
+                                :authorize_path => "/1/OAuthAuthorizeToken" }
+
+      uid do
+        raw_info['id']
+      end
+
+      info do
+        {
+          :name => raw_info['fullName']
+        }
+      end
+
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get('/1/members/me').body)
+      end
+    end
+  end
+end
