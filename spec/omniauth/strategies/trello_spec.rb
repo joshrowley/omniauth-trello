a1ac0539375f907f0637d0aeeabdf3aec2113b17
Simeon F. Willbanks <simeon@simeons.net>
2021-08-19 05:24:04 -0700

    Use RSpec expect syntax

--- spec/omniauth/strategies/trello_spec.rb
+++ spec/omniauth/strategies/trello_spec.rb
@@ -8,19 +8,19 @@ describe OmniAuth::Strategies::Trello do
 
   context "client options" do
     it "should have correct site" do
-      subject.options.client_options.site.should eq("https://trello.com")
+      expect(subject.options.client_options.site).to eq("https://trello.com")
     end
 
     it "should have correct authorize path" do
-      subject.options.client_options.authorize_path.should eq("/1/OAuthAuthorizeToken")
+      expect(subject.options.client_options.authorize_path).to eq("/1/OAuthAuthorizeToken")
     end
 
     it "should have the correct request token path" do
-      subject.options.client_options.request_token_path.should eq("/1/OAuthGetRequestToken")
+      expect(subject.options.client_options.request_token_path).to eq("/1/OAuthGetRequestToken")
     end
 
     it "should have the correct access token path" do
-      subject.options.client_options.access_token_path.should eq("/1/OAuthGetAccessToken")
+      expect(subject.options.client_options.access_token_path).to eq("/1/OAuthGetAccessToken")
     end
   end
 end
